0422feb5d03f2df0b9c53bc34d7a93b86d10c4a8
wooddang-gachon <al457616@gachon.ac.kr>
2026-09-24 14:46:31 +0900

    first init_wooddang

---
+++ kanghyeonwoo/src/main/java/com/leets_8th_be/kanghyeonwoo/KanghyeonwooApplication.java
@@ -0,0 +1,13 @@
+package com.leets_8th_be.kanghyeonwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanghyeonwooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanghyeonwooApplication.class, args);
+	}
+
+}
---
+++ kanghyeonwoo/src/test/java/com/leets_8th_be/kanghyeonwoo/KanghyeonwooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets_8th_be.kanghyeonwoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanghyeonwooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
